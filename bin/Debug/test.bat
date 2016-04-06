2503cb3425ea484f9aa804668f48f4e8ecbfcf18
surfsky <surfsky@sina.com>
2016-04-06 11:32:01 +0800

    add bin folder(with demo .bat file)

---
+++ bin/Debug/test.bat
@@ -0,0 +1,6 @@
+## linecounter.exe E:\Coding\WztelecomWeb\0703.DataMarket\DataMarket.2.0
+## linecounter.exe E:\Coding\KevinStudio\Kingsoc.Utility\Kingsoc.Utility.3
+
+
+linecounter.exe \\Mac\Home\Downloads\Dev\Dev-Qt\CODE\Game\AirCraft  codes.txt  9000
+
